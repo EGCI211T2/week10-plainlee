c2e43358e516e8a0a57062a5a1bc350f06b1f209
plainlee <plain.lee@student.mahidol.edu>
2026-03-16 08:41:16 +0000

    Fixed 1st attempt

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(){
     struct Time t1,t2,t3;
-    cout<<"What time was it?";
+    cout<<"What time was it?\n";
     getTime(t1);
-    cout<<"What time is it now?";
+    cout<<"What time is it now?\n";
     getTime(t2);
     t3=subtract(t2,t1); //t3=t2-t1
     cout<<"Time diff is ";
